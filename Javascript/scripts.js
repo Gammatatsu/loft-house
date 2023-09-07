f0eff16989acb3008d59aa6f8f5eeb15ecd91d80
Дмитрий Пивоваров <you@example.com>
2023-09-07 18:36:46 +0300

    ver5

---
+++ Javascript/scripts.js
@@ -0,0 +1,5 @@
+let menuButton = document.querySelector('.header__navigation-button')
+
+menuButton.addEventListener('click',() =>{
+    menuButton.classList.toggle('header__navigation-button--active')
+});
