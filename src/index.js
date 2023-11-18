acd7d684bd7b192a94c2febd514b0a4ff796a514
Дмитрий Пивоваров <dmitrypivovarov2001@gmail.com>
2023-11-18 23:09:31 +0300

    c webpack

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import 'normalize.css'
+import './styles/main.scss';
+
+let menuButton = document.querySelector('.header__navigation-button')
+let  headerMenu = document.querySelector('.header__navigation-list')
+ 
+menuButton.addEventListener('click',() =>{
+    menuButton.classList.toggle('header__navigation-button--active')
+    headerMenu.classList.toggle('header__navigation-list--active')
+    if(headerMenu.classList.contains('header__navigation-list--active')){
+        document.body.style.overflowY = 'hidden'
+    }
+    else{
+        document.body.style.overflowY = 'unset'
+    }
+});
